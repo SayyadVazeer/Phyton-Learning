87d686ddd60ee20de7f8f499136452d427ad45fe
SayyadVazeer <sayyadvazeer07@gmail.com>
2025-07-12 21:29:11 +0530

    Bank Project bug Fix

--- Main/chck.py
+++ Main/chck.py
@@ -1,3 +1,14 @@
-class chek :
-    
-    print( "Seems Working ")
+from abc import ABC , abstractmethod
+
+class Shape(ABC):
+
+    @abstractmethod
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+        print(f"{self.radius}")
+
+Circle(3)
